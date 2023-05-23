93e989f4c6a1e970477e2b261ad7d89f1323aa72
Ivan Krasnoshchok <ivankras67@gmail.com>
2023-05-23 12:07:31 +0300

    Update code to match naming conventions

--- bot.py
+++ bot.py
@@ -6,74 +6,72 @@ from telegram.ext import CommandHandler, MessageHandler, ApplicationBuilder, fil
 
 tmdb.API_KEY = os.getenv("TMDB_API_KEY")
 
-app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
+TEMP_LANG = "en"
+SHOW_POPULAR = True
 
-temp_lang = "en"
-show_popular = True
-
-answerAll = {
+ANSWER_ALL = {
     "en": "All found films will be shown",
     "uk": "Усі знайдені фільми будуть відображені",
 }
-answerPopular = {
+ANSWER_POPULAR = {
     "en": "Only the most popular films will be shown",
     "uk": "Тільки айпопулярніші фільми будуть відображені",
 }
 
 
 async def english(update, context):
-    global temp_lang
-    temp_lang = "en"
+    global TEMP_LANG
+    TEMP_LANG = "en"
     await update.message.reply_text("Language results are shown in has been changed successfully!/help")
     pass
 
 
 async def ukrainian(update, context):
-    global temp_lang
-    temp_lang = "uk"
+    global TEMP_LANG
+    TEMP_LANG = "uk"
     await update.message.reply_text("Мову відображення результатів успішно змінено!/help")
     pass
 
 
-async def helpMessage(update, context):
-    changeLanguage = "To change language:\n/en - English" + "\n/uk - Українська"
-    Filter = (
+async def help_message(update, context):
+    change_language = "To change language:\n/en - English" + "\n/uk - Українська"
+    filter = (
         "\nTo filter by popularity:\n/showall - "
-        + answerAll[temp_lang]
+        + ANSWER_ALL[TEMP_LANG]
         + "\n/showpopular - "
-        + answerPopular[temp_lang]
+        + ANSWER_POPULAR[TEMP_LANG]
     )
-    await update.message.reply_text(changeLanguage + Filter)
+    await update.message.reply_text(change_language + filter)
     pass
 
 
-async def showPopular(update, context):
-    global show_popular
-    show_popular = True
-    await update.message.reply_text(answerPopular[temp_lang] + "(max = 5)/help")
+async def show_popular(update, context):
+    global SHOW_POPULAR
+    SHOW_POPULAR = True
+    await update.message.reply_text(ANSWER_POPULAR[TEMP_LANG] + "(max = 5)/help")
     pass
 
 
-async def showAll(update, context):
-    global show_popular
-    show_popular = False
-    await update.message.reply_text(answerAll[temp_lang] + "(unlimited)/help")
+async def show_all(update, context):
+    global SHOW_POPULAR
+    SHOW_POPULAR = False
+    await update.message.reply_text(ANSWER_ALL[TEMP_LANG] + "(unlimited)/help")
     pass
 
 
-async def startMessaging(update, context):
+async def start_messaging(update, context):
     await update.message.reply_text("Привіт, чого бажаєте?/help")
     pass
 
 
-async def updateMessage(update, context):
+async def update_message(update, context):
     search = tmdb.Search()
     searched_film = update.message.text.lower()
-    search.movie(query=searched_film, language=temp_lang)
+    search.movie(query=searched_film, language=TEMP_LANG)
 
     if not search.results:
         answer = {"en": "No result. Please, change your query", "uk": "Нічого не знайдено. Будь ласка, змініть запит"}
-        await update.message.reply_text(answer[temp_lang] + "/help")
+        await update.message.reply_text(answer[TEMP_LANG] + "/help")
         pass
 
     res = sorted(search.results, key=lambda k: k["popularity"], reverse=True)
@@ -81,7 +79,7 @@ async def updateMessage(update, context):
     counter = 0
     k = 0
     for i in res:
-        if k == 5 and show_popular == True:
+        if k == 5 and SHOW_POPULAR == True:
             break
         name = str(i["title"]).lower()
         if name.find(searched_film) == -1 and counter != 0:
@@ -109,13 +107,16 @@ async def updateMessage(update, context):
     pass
 
 
-app.add_handler(CommandHandler("start", startMessaging))
-app.add_handler(CommandHandler("showpopular", showPopular))
-app.add_handler(CommandHandler("showall", showAll))
-app.add_handler(CommandHandler("help", helpMessage))
+app = ApplicationBuilder().token(os.getenv("TELEGRAM_BOT_TOKEN")).build()
+
+app.add_handler(CommandHandler("start", start_messaging))
+app.add_handler(CommandHandler("showpopular", show_popular))
+app.add_handler(CommandHandler("showall", show_all))
+app.add_handler(CommandHandler("help", help_message))
 app.add_handler(CommandHandler("en", english))
 app.add_handler(CommandHandler("uk", ukrainian))
-app.add_handler(MessageHandler(filters.TEXT, updateMessage))
+app.add_handler(MessageHandler(filters.TEXT, update_message))
 
 
-app.run_polling()
+if __name__ == "__main__":
+    app.run_polling()
