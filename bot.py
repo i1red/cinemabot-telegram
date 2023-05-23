e46243656d884e9ac9b0e55f7dda3f5c0edec4fc
Ivan Krasnoshchok <ivankras67@gmail.com>
2023-05-23 11:46:43 +0300

    Update code to new python-telegram-bot (v20.3)
    Build app using ApplicationBuilder
    Use `async def (update, context)` callbacks
    instead of `def (bot, update)`
    Add pyproject.toml dependency file

--- bot.py
+++ bot.py
@@ -1,73 +1,67 @@
-﻿import telegram
+﻿import os
+
 import tmdbsimple as tmdb
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.constants import ParseMode
+from telegram.ext import CommandHandler, MessageHandler, ApplicationBuilder, filters
 
-tmdb.API_KEY = 'c8614c46f861944cdcedd2e79fdd1a20'
+tmdb.API_KEY = os.getenv('TMDB_API_KEY')
 
-updater = Updater('718242667:AAEUaltXy6raqTmsRxBtVtJvWJNmzkgoW5I')
-dispatcher = updater.dispatcher
+app = ApplicationBuilder().token(os.getenv('TELEGRAM_BOT_TOKEN')).build()
 
 temp_lang = 'en'
 show_popular = True
 
-answerAll = {'en': 'All found films will be shown', 'uk' : 'Усі знайдені фільми будуть відображені', 'zh' : '將顯示所有找到的電影', 'ru' : 'Все найденые фыльмы будут отображены'}
-answerPopular = {'en': 'Only the most popular films will be shown', 'uk' : 'Тільки айпопулярніші фільми будуть відображені', 'zh' : '只會播放最受歡迎的電影', 'ru' : 'Только самые популярные фыльмы будут отображены'}
+answerAll = {'en': 'All found films will be shown', 'uk' : 'Усі знайдені фільми будуть відображені', 'zh' : '將顯示所有找到的電影'}
+answerPopular = {'en': 'Only the most popular films will be shown', 'uk' : 'Тільки айпопулярніші фільми будуть відображені', 'zh' : '只會播放最受歡迎的電影'}
 
-def english(bot, update):
+async def english(update, context):
     global temp_lang
     temp_lang = 'en'
-    bot.send_message(chat_id=update.message.chat_id, text='Language results are shown in has been changed successfully!/help')
+    await update.message.reply_text('Language results are shown in has been changed successfully!/help')
     pass
 
-def ukrainian(bot, update):
+async def ukrainian(update, context):
     global temp_lang
     temp_lang = 'uk'
-    bot.send_message(chat_id=update.message.chat_id, text='Мову відображення результатів успішно змінено!/help')
+    await update.message.reply_text('Мову відображення результатів успішно змінено!/help')
     pass
 
-def chinese(bot, update):
+async def chinese(update, context):
     global temp_lang
     temp_lang = 'zh'
-    bot.send_message(chat_id=update.message.chat_id, text='語言結果顯示已成功更改!/help')
+    await update.message.reply_text('語言結果顯示已成功更改!/help')
     pass
 
-def russian(bot, update):
-    global temp_lang
-    temp_lang = 'ru'
-    bot.send_message(chat_id=update.message.chat_id, text='Язык выдачи результатов успешно изменен!/help')
-    pass
-
-def helpMessage(bot, update):
-    changeLanguage = 'To change language:\n/en - English' + '\n/zh - 中國' + '\n/uk - Українська' + '\n/ru - Русский'
+async def helpMessage(update, context):
+    changeLanguage = 'To change language:\n/en - English' + '\n/zh - 中國' + '\n/uk - Українська'
     Filter = '\nTo filter by popularity:\n/showall - ' + answerAll[temp_lang] + '\n/showpopular - ' + answerPopular[temp_lang] 
-    bot.send_message(chat_id=update.message.chat_id, text=changeLanguage+Filter)
+    await update.message.reply_text(changeLanguage+Filter)
     pass
 
-def showPopular(bot, update):
+async def showPopular(update, context):
     global show_popular
     show_popular = True
-    bot.send_message(chat_id=update.message.chat_id, text=answerPopular[temp_lang] + '(max = 5)/help')
+    await update.message.reply_text(answerPopular[temp_lang] + '(max = 5)/help')
     pass
 
-def showAll(bot, update):
+async def showAll(update, context):
     global show_popular
     show_popular = False
-    bot.send_message(chat_id=update.message.chat_id, text=answerAll[temp_lang] + '(unlimited)/help')
+    await update.message.reply_text(answerAll[temp_lang] + '(unlimited)/help')
     pass
 
-def startMessaging(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Здравствуйте, чего желаете?/help')
+async def startMessaging(update, context):
+    await update.message.reply_text('Привіт, чого бажаєте?/help')
     pass
 
-def updateMessage(bot, update):
-    message_id = update.message.chat_id
+async def updateMessage(update, context):
     search = tmdb.Search()
     searched_film = update.message.text.lower()
     search.movie(query=searched_film, language=temp_lang)
 
     if not search.results:
-        answer = {'en': 'No result. Please, change your query', 'uk' : 'Нічого не знайдено. Будь ласка, змініть запит', 'zh' : '沒有結果。 請更改您的查詢', 'ru' : 'Ничего не найдено. Пожалуйста измените запрос'}
-        bot.send_message(chat_id=message_id, text=answer[temp_lang] + '/help')
+        answer = {'en': 'No result. Please, change your query', 'uk' : 'Нічого не знайдено. Будь ласка, змініть запит', 'zh' : '沒有結果。 請更改您的查詢'}
+        await update.message.reply_text(answer[temp_lang] + '/help')
         pass
     
     res = sorted(search.results, key = lambda k: k['popularity'], reverse = True)
@@ -84,31 +78,20 @@ def updateMessage(bot, update):
             counter+=1
         film_info = '\t'  + "*" + str(i['title'])  + ', ' + str(i['release_date']) + ', ' + str(i['vote_average']) + '/10' + "*" + u"\U0001F37F" + '\n\t' + str(i['overview'])
         poster_path = 'http://image.tmdb.org/t/p/w780' + str(i['poster_path'])
-        bot.send_message(chat_id=message_id, text=film_info, parse_mode=telegram.ParseMode.MARKDOWN)
-        bot.send_photo(chat_id=message_id, photo=poster_path)
+        await update.message.reply_text(film_info, parse_mode=ParseMode.MARKDOWN)
+        await update.message.reply_photo(poster_path)
         k+=1
     pass
 
-start_command_handler = CommandHandler('start', startMessaging)
-showPopular_command_handler = CommandHandler('showpopular', showPopular)
-showAll_command_handler = CommandHandler('showall', showAll)
-help_command_handler = CommandHandler('help', helpMessage)
-english_command_handler = CommandHandler('en', english)
-ukrainian_command_handler = CommandHandler('uk', ukrainian)
-chinese_command_handler = CommandHandler('zh', chinese)
-russian_command_handler = CommandHandler('ru', russian)
-text_message_handler = MessageHandler(Filters.text, updateMessage)
+app.add_handler(CommandHandler('start', startMessaging))
+app.add_handler(CommandHandler('showpopular', showPopular))
+app.add_handler(CommandHandler('showall', showAll))
+app.add_handler(CommandHandler('help', helpMessage))
+app.add_handler(CommandHandler('en', english))
+app.add_handler(CommandHandler('uk', ukrainian))
+app.add_handler(CommandHandler('zh', chinese))
+app.add_handler(MessageHandler(filters.TEXT, updateMessage))
 
-dispatcher.add_handler(start_command_handler)
-dispatcher.add_handler(showPopular_command_handler)
-dispatcher.add_handler(showAll_command_handler)
-dispatcher.add_handler(help_command_handler)
-dispatcher.add_handler(english_command_handler)
-dispatcher.add_handler(ukrainian_command_handler)
-dispatcher.add_handler(russian_command_handler)
-dispatcher.add_handler(chinese_command_handler)
-dispatcher.add_handler(text_message_handler)
 
-updater.start_polling(clean=True)
+app.run_polling()
 
-updater.idle()
